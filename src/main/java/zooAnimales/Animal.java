199141862caf5585826efd97ee69a0776efa6fc4
bosettii <80700251+bosettii@users.noreply.github.com>
2023-09-15 19:33:27 -0500

    v3.4

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -86,9 +86,9 @@ public class Animal {
 
     public String toString(){
         if(this.zona != null)
-            return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en" + this.habitat + ", y mi genero es " + this.genero ;
+            return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en" + this.getHabitat() + ", y mi genero es " + this.getGenero() ;
             
-        return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en" + this.habitat + ", y mi genero es " + this.genero + ", la zona en la que me ubico es" + this.zona + ", en el zoo" + this.zona.getZoo();
+        return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en" + this.getHabitat() + ", y mi genero es " + this.getGenero() + ", la zona en la que me ubico es" + this.getZona() + ", en el zoo" + this.zona.getZoo();
     }
     
 }
